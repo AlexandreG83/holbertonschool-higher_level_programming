19207f9fb5bda7d54601a267d01b81a0b2404af8
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-01-14 08:48:14 +0000

    no tabs, if chr(i) != 'e' and chr(i) != 'q':

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 """Print the ASCII lowercase alphabet without q and e in one line"""
 for i in range(97, 123):
-	if chr(i) != 'e' and chr(i) != 'q':
-		print("{}".format(chr(i)), end="")
+    if chr(i) != 'e' and chr(i) != 'q':
+        print("{}".format(chr(i)), end="")
