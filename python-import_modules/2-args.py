6c6821048de2762d70ca1e41151f7c2f92ca7aa6
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-01-16 12:53:45 +0000

    cases where argc = 1 or argc = 2 -> print directly '0 arguments' '1 arguments'

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,6 +2,12 @@
 import sys
 if __name__ == "__main__":
     argc = len(sys.argv) - 1
-    print("{} arguments:".format(argc))
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(argc))
+
     for i in range(argc):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
