5c996c98f7c15f222c6877bd7b5a76d7ad4e85d5
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-01-23 13:49:27 +0000

    -div must be a number; no egal to 0, else ZeroDivisionError -matrix must be a matrix of integers/floats -> if (not isinstance(matrix, list) or any(not isinstance(row, list) for row in matrix)): -length of the row -> lenght of matrix at index 0 if matrix, else row_length = 0 -create new matrix empty -for each row in matrix  (each row of the matrix must have the same size)-> create a new row (empty) -> for each item in row -> new_row.append(round(item / div, 2)) 	-new_matrix.append(new_row) -return the new_matrix; and pycodestyle

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -37,8 +37,7 @@ def matrix_divided(matrix, div):
         for item in row:
             if not isinstance(item, (int, float)):
                 raise TypeError(
-                    "matrix must be a matrix (list of lists)"
-                    "of integers/floats")
+                    "matrix must be a matrix (list of lists) of integers/floats"
             new_row.append(round(item / div, 2))
         new_matrix.append(new_row)
     return new_matrix
