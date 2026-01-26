3dc335b92b1bb0b3fbd03e6bcd8db6f655ff2e8f
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-01-26 01:04:30 +0000

    - def __init__ to initialize a new square, and the arg size of the square - size = self.__size

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a Square class."""
+
+
+class Square:
+    """Represents a square."""
+    def __init__(self, size):
+        """Initializes a new Square.
+        Args:
+            size: size of the square
+        """
+        self.__size = size
