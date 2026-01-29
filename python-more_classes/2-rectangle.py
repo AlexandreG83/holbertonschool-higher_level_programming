c22b5a7818ce03ee1e7a75a91d659eb99bd19b78
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-01-29 08:50:15 +0000

    requierement fail

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -41,11 +41,3 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return (self.__height + self.__width) * 2
-
-    def __str__(self):
-        if self.__width == 0 or self.__height == 0:
-            return ""
-        lines = []
-        for _ in range(self.__height):
-            lines.append("#" * self.__width)
-        return "\n".join(lines)
