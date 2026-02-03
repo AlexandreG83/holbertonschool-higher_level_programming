29f26a519b6533eece4af1111026e60b99301737
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-02-03 14:21:13 +0000

    -def __str__ -> return print the rectangle width and rectangle height

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module that defines a Square class inheriting from Rectangle"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Square(Rectangle):
+    """Square class inheriting from Rectangle"""
+
+    def __init__(self, size):
+        """Initialize a square with size"""
+        BaseGeometry.integer_validator(self, "size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        """Return string representation of the square"""
+        return "[Square] {}/{}".format(self._Rectangle__width,
+                                       self._Rectangle__height)
