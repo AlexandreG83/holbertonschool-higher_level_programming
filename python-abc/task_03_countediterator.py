961e3d4a885b8452f7173ddbe7af340219f1601c
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-02-04 12:25:21 +0000

    class CountedIterator (non-inherited) def __next__ -item = next(self.iterator) -> if it's finished -> StopIteration (not catch it) -count + 1 if an item is recovered return the count

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Defines a CountedIterator that counts iterated items"""
+
+
+class CountedIterator:
+    """Iterator that counts how many items have been iterated"""
+
+    def __init__(self, iterable):
+        self.iterator = iter(iterable)
+        self.count = 0
+
+    def __next__(self):
+        item = next(self.iterator)
+        self.count += 1
+        return item
+
+    def get_count(self):
+        return self.count
