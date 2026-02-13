6cdcf8048aea962d181b4720d58f16da671ea2b5
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-02-13 14:10:07 +0000

    return the dict

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Module that provides a function to get a dictionary representation of a class instance."""
+"""Module that provides a function to get a dictionary representation
+of a class instance."""
 
 
 def class_to_json(obj):
-    """Returns the dictionary description of a class instance for JSON serialization."""
+    """Returns the dictionary description of a class instance
+    for JSON serialization."""
     return obj.__dict__
