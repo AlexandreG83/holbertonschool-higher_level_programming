1557762d2b50959b547c8c00e5bba5d4481a896c
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-03-06 14:53:46 +0100

    update names

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- Lists all genres with the number of shows linked
-SELECT g.name AS genre, COUNT(tsg.show_id) AS number_of_shows
-FROM g
-JOIN tsg ON g.id = tsg.genre_id
-GROUP BY g.name
+SELECT tv_genre.name AS genre, COUNT(tv_show_genre.show_id) AS number_of_shows
+FROM tv_genre
+JOIN tv_show_genre ON tv_genre.id = tv_show_genre.genre_id
+GROUP BY tv_genre.name
 ORDER BY number_of_shows DESC;
