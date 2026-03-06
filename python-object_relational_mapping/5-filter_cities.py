5b40052177943ece8e8f63a0bf7bb870d1704469
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-03-06 13:45:06 +0100

    Extract city names and print in one line separated by comma
    Extract city names and print in one line separated by comma

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Lists all cities of a given state (SQL injection safe)
+Usage: ./5-filter_cities.py <username> <password> <database> <state_name>
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cur = db.cursor()
+
+    query = """
+        SELECT cities.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+    """
+    cur.execute(query, (state_name,))
+
+    rows = cur.fetchall()
+
+    cities = [row[0] for row in rows]
+    print(", ".join(cities))
+
+    cur.close()
+    db.close()
