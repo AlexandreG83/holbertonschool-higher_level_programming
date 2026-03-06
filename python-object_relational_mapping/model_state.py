39cd67136da63204beada77051123a77c792cb54
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-03-06 13:46:21 +0100

    Base ORM, creates a Python class that represents a SQL table, creates a column id with properties

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines the State class and Base"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """State class that links to the states table"""
+
+    __tablename__ = "states"
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
