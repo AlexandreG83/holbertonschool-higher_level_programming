ce91e2c4badb5461b4bdc0ac79fb4634e80f4f9b
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-03-06 13:43:58 +0100

    Connect to MySQL server and Secure query using parameterized statement
    This script connects to a MySQL database and safely retrieves states matching a given name, protecting against SQL injection.

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Displays all values in states where name matches the argument
+Safe from SQL injection
+Usage: ./3-my_safe_filter_states.py <username> <password> <database> <state_name>
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cur = db.cursor()
+
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cur.execute(query, (state_name,))
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
