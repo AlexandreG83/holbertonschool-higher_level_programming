b3105fe026577362b2c4d4fb4e22a44e4a74fdf3
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-03-06 14:58:15 +0100

    Update names

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,7 @@
 -- Lists all genres of the show Dexter
-SELECT g.name
-FROM tv_shows s
-JOIN tv_show_genres tsg ON s.id = tsg.tv_show_id
-JOIN genres g ON tsg.genre_id = g.id
-WHERE s.title = 'Dexter'
-ORDER BY g.name ASC;
+SELECT tv_genres.name
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
