59fac59f5963a718c20894cbb14214761b12379d
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-03-06 13:37:26 +0100

    - WHERE name = '{}'.format(state_name)  → insère directement l’argument utilisateur dans la requête. - ORDER BY id ASC  → tri croissant.
    - WHERE name = '{}'.format(state_name)  → insère directement l’argument utilisateur dans la requête.
    - ORDER BY id ASC  → tri croissant.

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Displays all values in states where name matches the argument
+Usage: ./2-my_filter_states.py <username> <password> <database> <state_name>
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cur = db.cursor()
+
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+
+    cur.execute(query)
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
