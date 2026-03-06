37b4bf9f8d20e705087f41d9ec4bf5f859b36b3a
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-03-06 15:03:21 +0100

    Update 15-comedy_only.sql

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,6 +1,7 @@
 -- Lists all shows with the genre Comedy
-SELECT tv_shows.title, tv_genres.name
+SELECT tv_shows.title
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-ORDER BY tv_shows.title ASC, tv_genres.name ASC;
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title ASC;
