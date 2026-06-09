1d410b7e354d38bc81dc173246710249ccbd925f
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-06-09 10:03:24 +0200

    modify query

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
 Lists all cities of a given state (SQL injection safe)
-Usage: ./5-filter_cities.py <username> <password> <database> <state_name>
+Usage: ./5-filter_cities.py <username>
+    <password> <database> <state_name>
 """
 import MySQLdb
 import sys
@@ -18,17 +19,18 @@ if __name__ == "__main__":
         user=username,
         passwd=password,
         db=database
+        charset="utf8"
     )
 
     cur = db.cursor()
 
-    query = """
-        SELECT cities.name
-        FROM cities
-        JOIN states ON cities.state_id = states.id
-        WHERE states.name = %s
-        ORDER BY cities.id ASC
-    """
+    query = (
+        "SELECT cities.name "
+        "FROM cities "
+        "JOIN states ON cities.state_id = states.id "
+        "WHERE states.name = %s "
+        "ORDER BY cities.id ASC"
+    )
     cur.execute(query, (state_name,))
 
     rows = cur.fetchall()
