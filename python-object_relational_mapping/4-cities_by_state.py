2d5544af7926161438e7a11b1073a2c7ce51f756
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-06-09 10:00:42 +0200

    modify query

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -17,16 +17,17 @@ if __name__ == "__main__":
         user=username,
         passwd=password,
         db=database
+        charset="utf8"
     )
 
     cur = db.cursor()
 
-    query = """
-        SELECT cities.id, cities.name, states.name
-        FROM cities
-        JOIN states ON cities.state_id = states.id
-        ORDER BY cities.id ASC
-    """
+    query = (
+        "SELECT cities.id, cities.name, states.name "
+        "FROM cities "
+        "JOIN states ON cities.state_id = states.id "
+        "ORDER BY cities.id ASC;"
+    )
     cur.execute(query)
 
     rows = cur.fetchall()
