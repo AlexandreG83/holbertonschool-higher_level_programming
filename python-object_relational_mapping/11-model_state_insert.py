921e898057f4eabead8224561e9c84b0baa36a6a
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-06-11 09:53:51 +0200

    pycodestyle

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -8,14 +8,14 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-    username = sys.argv[1]  
-    password = sys.argv[2]  
-    database = sys.argv[3]  
-  
-    engine = create_engine(  
-        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(  
-            username, password, database),  
-        pool_pre_ping=True  
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            username, password, database),
+        pool_pre_ping=True
     )
 
     Session = sessionmaker(bind=engine)
