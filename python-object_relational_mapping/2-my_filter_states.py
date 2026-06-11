c2ad6bbfff2ac05a5bf85fb0819fe4855362cb73
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-06-11 09:42:57 +0200

    cur.fetchall() in the loop

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,38 +1,41 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 """
-Displays all values in states where name matches the argument
-Usage: ./2-my_filter_states.py <username> <password> <database> <state_name>
+List states matching a provided name from a MySQL database.
+
+Usage:
+    ./2-my_filter_states.py <user> <password> <database> <state_name>
 """
-import MySQLdb
+
 import sys
+import MySQLdb
+
 
 if __name__ == "__main__":
-    username = sys.argv[1]
+    user = sys.argv[1]
     password = sys.argv[2]
-    database = sys.argv[3]
+    db_name = sys.argv[3]
     state_name = sys.argv[4]
 
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=username,
+        user=user,
         passwd=password,
-        db=database
+        db=db_name,
         charset="utf8"
     )
-
     cur = db.cursor()
 
     query = (
-        "SELECT * FROM states"
-        "WHERE BINARY name = '{}'"
-        "ORDER BY states.id ASC"
+        "SELECT * FROM states "
+        "WHERE BINARY name = '{}' "
+        "ORDER BY states.id ASC;"
     ).format(state_name)
 
     cur.execute(query)
 
-    rows = cur.fetchall()
-    for row in rows:
+    for row in cur.fetchall():
         print(row)
 
     cur.close()
