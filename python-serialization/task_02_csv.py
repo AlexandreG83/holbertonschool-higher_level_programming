8f5999088cfcac414692b42dd482e9183fbdc5b5
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-06-25 10:19:19 +0200

    refactor: rename csv_file and json_file variables to file

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -16,12 +16,12 @@ def convert_csv_to_json(csv_filename):
         bool: True if conversion is successful, False otherwise.
     """
     try:
-        with open(csv_filename, mode='r', encoding='utf-8') as csv_file:
-            reader = csv.DictReader(csv_file)
+        with open(csv_filename, mode='r', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
             data = [row for row in reader]
 
-        with open('data.json', mode='w', encoding='utf-8') as json_file:
-            json.dump(data, json_file, indent=4)
+        with open('data.json', mode='w', encoding='utf-8') as file:
+            json.dump(data, file, indent=4)
 
         return True
 
