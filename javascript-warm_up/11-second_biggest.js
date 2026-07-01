74b305aef1780f55cb7eacef6035fa47d85d2bb1
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-07-01 12:56:47 +0200

    semistandard respected

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -2,20 +2,16 @@
 
 const argc = process.argv.slice(2).map(Number);
 
-if (argc.length < 2) {console.log(0);}
-else
-{
+if (argc.length < 2) {
+  console.log(0);
+} else {
   let max = -Infinity;
   let secondMax = -Infinity;
-  for (let i = 0; i < argc.length; i++)
-  {
-    if (argc[i] > max)
-    {
+  for (let i = 0; i < argc.length; i++) {
+    if (argc[i] > max) {
       secondMax = max;
       max = argc[i];
-    }
-    else if (argc[i] > secondMax && argc[i] !== max)
-    {
+    } else if (argc[i] > secondMax && argc[i] !== max) {
       secondMax = argc[i];
     }
   }
