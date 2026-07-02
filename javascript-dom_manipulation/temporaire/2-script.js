1eb37ecd7bcfab5b506bac301185ed20d1d1c44f
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-07-02 10:19:05 +0200

    0

--- javascript-dom_manipulation/temporaire/2-script.js
+++
@@ -1,6 +0,0 @@
-const header = document.querySelector(header);
-const redHeader = document.querySelector(#red_header);
-
-redHeader.addEventListener(click, function () {
-  header.classList.add(red);
-});
--- javascript-dom_manipulation/temporaire/5-script.js
+++
@@ -1,6 +0,0 @@
-const header = document.querySelector('header');
-const updateHeader = document.querySelector('#update_header');
-
-updateHeader.addEventListener('click', function () {
-  header.textContent = 'New Header!!!';
-});
